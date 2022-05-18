01302c72737757a57a399d75ec14d7a971173cf6
hackprotech <78909147+hackprotech@users.noreply.github.com>
2022-05-18 15:56:54 +0530

    Final Changes

---
+++ final/spark-loader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Settings ENV in .bashrc file
+echo "export JAVA_HOME=/home/bigdata/openlogic-openjdk-8u262-b10-linux-64" >> /root/.bashrc
+echo "export PATH=$PATH:/home/bigdata/openlogic-openjdk-8u262-b10-linux-64/bin" >> /root/.bashrc
+source /root/.bashrc
+echo ">>>>>>> Setting ENV variables done..."
+
+# Start the ssh server
+echo ">>>>>> Starting the SSH server..."
+/etc/init.d/ssh start
+
+echo ">>>>>> Starting the Apache Spark..."
+./spark-3.1.2-bin-hadoop2.7/sbin/start-all.sh
+
+jps
+
+tail -f /dev/null
