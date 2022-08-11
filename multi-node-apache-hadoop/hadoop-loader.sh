31d0c9f968d74375a8ca0637ce0a9c8febf91541
vengatesanns <vengat19amd@gmail.com>
2022-08-11 22:18:55 +0530

    Scripts refactored

---
+++ multi-node-apache-hadoop/hadoop-loader.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Settings ENV in .bashrc file
+echo "export JAVA_HOME=/home/bigdata/openlogic-openjdk-8u262-b10-linux-64" >> /root/.bashrc
+echo "export PATH=$PATH:/home/bigdata/openlogic-openjdk-8u262-b10-linux-64/bin" >> /root/.bashrc
+source /root/.bashrc
+echo ">>>>>>> Setting ENV variables done..."
+
+# Start the ssh server
+echo ">>>>>> Starting the SSH server..."
+/etc/init.d/ssh start
+
+# Start the Master and Worker based on the env params condition
+echo ">>>>>> Starting the Apache Spark..."
+if [ $NODE_TYPE == "MASTER" ]
+then
+    HOST_MACHINE_IP_ADDRESS=$(hostname -i)
+    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+    echo "SPARK_MASTER_NODE - $NODE_TYPE"
+    ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
+elif  [ $NODE_TYPE == "WORKER" ]
+then
+    HOST_MACHINE_IP_ADDRESS=$(ping spark-master -c1 | head -1 | grep -Eo '[0-9.]{4,}')
+    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+    echo "SPARK_WORKER_NODE - $NODE_TYPE"
+    ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
+else
+    echo "Invalid NODE_TYPE - $NODE_TYPE"
+fi
+
+tail -f /dev/null
