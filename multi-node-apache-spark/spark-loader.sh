3003931460300f3fa5cac3950b16600049f8370b
vengatesanns <vengat19amd@gmail.com>
2022-08-13 12:31:08 +0530

    Node Type Updated

--- multi-node-apache-spark/spark-loader.sh
+++ multi-node-apache-spark/spark-loader.sh
@@ -12,20 +12,20 @@ echo ">>>>>> Starting the SSH server..."
 
 # Start the Master and Worker based on the env params condition
 echo ">>>>>> Starting the Apache Spark..."
-if [ $SPARK_NODE == "MASTER" ]
+if [ $NODE_TYPE == "MASTER" ]
 then
     HOST_MACHINE_IP_ADDRESS=$(hostname -i)
     echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-    echo "SPARK_MASTER_NODE - $SPARK_NODE"
+    echo "SPARK_MASTER_NODE - $NODE_TYPE"
     ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
-elif  [ $SPARK_NODE == "WORKER" ]
+elif  [ $NODE_TYPE == "WORKER" ]
 then
     HOST_MACHINE_IP_ADDRESS=$(ping spark-master -c1 | head -1 | grep -Eo '[0-9.]{4,}')
     echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-    echo "SPARK_WORKER_NODE - $SPARK_NODE"
+    echo "SPARK_WORKER_NODE - $NODE_TYPE"
     ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
 else
-    echo "Invalid SPARK_NODE - $SPARK_NODE"
+    echo "Invalid NODE_TYPE - $NODE_TYPE"
 fi
 
 tail -f /dev/null
