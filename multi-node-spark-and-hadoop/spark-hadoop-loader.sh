830848d2363edc947b4bbef6c666cf7b61142a96
vengatesanns <vengat19amd@gmail.com>
2022-08-14 10:13:51 +0530

    3 Node Cluster for Spark and Hadoop done

--- multi-node-spark-and-hadoop/spark-hadoop-loader.sh
+++ multi-node-spark-and-hadoop/spark-hadoop-loader.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Settings ENV in .bashrc file
 echo "export JAVA_HOME=/home/bigdata/openlogic-openjdk-8u262-b10-linux-64" >> /root/.bashrc
 echo "export PATH=$PATH:/home/bigdata/openlogic-openjdk-8u262-b10-linux-64/bin" >> /root/.bashrc
@@ -14,57 +13,42 @@ echo ">>>>>> SSH Server Started Successfully..."
 # Start the Master and Worker based on the env params condition
 echo " StrictHostKeyChecking no" >> ~/.ssh/config
 
-
-# Executing the Multinode Hadoop Cluster
-echo ">>>>>> Starting the Multinode Hadoop Cluster..."
-
 # Delete the configuration tag in xml
 sed -i "/<configuration>/,/<\/configuration>/d " hadoop-2.7.7/etc/hadoop/hdfs-site.xml
 
-if [ $FRAMEWORK == "HADOOP" ];
-then
-    if [ $NODE_TYPE == "MASTER" ]
-    then
-        rm hadoop-2.7.7/etc/hadoop/slaves
-        touch slaves
-        echo "slave-node-1" >> hadoop-2.7.7/etc/hadoop/slaves
-        echo "slave-node-2" >> hadoop-2.7.7/etc/hadoop/slaves
-        cat /home/bigdata/hdfs-site-master.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
+MASTER_NODE="master-node"
 
-        hadoop-2.7.7/bin/hdfs namenode -format
-        echo ">>>>> Hadoop Master Created Successfully!"
-    elif  [ $NODE_TYPE == "SLAVE" ]
-    then
-        cat /home/bigdata/hdfs-site-slave.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
-        echo ">>>>> Spark Slave Created Successfully!"
-    else
-        echo "Invalid HADOOP NODE_TYPE - $NODE_TYPE"
-    fi
-    
+if [ $NODE_TYPE == "MASTER" ]
+then
+    echo ">>>>>> Starting the Multinode Hadoop Cluster - MASTER NODE..."
+    rm hadoop-2.7.7/etc/hadoop/slaves
+    touch slaves
+    echo "slave-node-1" >> hadoop-2.7.7/etc/hadoop/slaves
+    echo "slave-node-2" >> hadoop-2.7.7/etc/hadoop/slaves
+    cat /home/bigdata/hdfs-site-master.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
+    hadoop-2.7.7/bin/hdfs namenode -format
     hadoop-2.7.7/sbin/start-all.sh
+    echo ">>>>> Hadoop Master Started Successfully!"
 
-elif [ $FRAMEWORK == "SPARK" ]
+    echo ">>>>>> Starting the Multinode Spark Cluster - MASTER NODE..."
+    HOST_MACHINE_IP_ADDRESS=$(hostname -i)
+    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+    ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
+    echo ">>>>> Spark Master Started Successfully!"
+
+elif  [ $NODE_TYPE == "SLAVE" ]
 then
-    # Executing the Multinode Spark Cluster
-    echo ">>>>>> Starting the Multinode Spark Cluster..."
-    if [ $NODE_TYPE == "MASTER" ]
-    then
-        HOST_MACHINE_IP_ADDRESS=$(hostname -i)
-        echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-        echo "SPARK_MASTER_NODE - $NODE_TYPE"
-        ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
-        echo ">>>>> Spark Master Created Successfully!"
-    elif  [ $NODE_TYPE == "WORKER" ]
-    then
-        HOST_MACHINE_IP_ADDRESS=$(ping spark-master -c1 | head -1 | grep -Eo '[0-9.]{4,}')
-        echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-        echo "SPARK_WORKER_NODE - $NODE_TYPE"
-        ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
-        echo ">>>>> Spark Worker Created Successfully!"
-    else
-        echo "Invalid SPARK NODE_TYPE - $NODE_TYPE"
-    fi
-fi
+    echo ">>>>> Starting Hadoop Slave Node - $NODE_NUM..."
+    cat /home/bigdata/hdfs-site-slave.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
+    echo ">>>>> Hadoop Slave Node - $NODE_NUM Started Successfully"
 
+    echo ">>>>> Starting Spark Slave Node - $NODE_NUM..."
+    HOST_MACHINE_IP_ADDRESS=$(ping $MASTER_NODE -c1 | head -1 | grep -Eo '[0-9.]{4,}')
+    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+    ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
+    echo ">>>>> Spark Slave Node - $NODE_NUM Started Successfully"
+else
+    echo "Invalid HADOOP NODE_TYPE - $NODE_TYPE"
+fi  
 
 tail -f /dev/null
