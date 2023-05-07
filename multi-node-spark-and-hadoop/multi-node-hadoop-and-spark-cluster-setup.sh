ed24280693ab2c48782d2bce8bdf69ce48549282
vengatesanns <vengat19amd@gmail.com>
2023-05-07 22:18:45 +0530

    sh file formatted and updated with proper if conditions

--- multi-node-spark-and-hadoop/multi-node-hadoop-and-spark-cluster-setup.sh
+++ multi-node-spark-and-hadoop/multi-node-hadoop-and-spark-cluster-setup.sh
@@ -3,54 +3,45 @@ ARG1=$1
 DOCKER_IMAGE_NAME=hackprotech/hadoop-and-spark-multinode-cluster:latest
 
 buildDockerImage() {
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>> Building the Multi-node Hadoop and Spark Cluster Image"
-    docker build -t $DOCKER_IMAGE_NAME .
-    docker images
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>> Building the Multi-node Hadoop and Spark Cluster Image"
+  docker build -t $DOCKER_IMAGE_NAME .
+  docker images
 }
 
 publishDockerImage() {
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>> Publishing the Multi-node Hadoop and Spark Cluster Image to Public Docker Hub"
-    docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME
-    docker push $DOCKER_IMAGE_NAME
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>> Publishing the Multi-node Hadoop and Spark Cluster Image to Public Docker Hub"
+  docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME
+  docker push $DOCKER_IMAGE_NAME
 }
 
 deployDockerImage() {
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>> Deploying the Multi-node Hadoop and Spark Cluster"
-    docker-compose up -d
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>> Deploying the Multi-node Hadoop and Spark Cluster"
+  docker compose up -d
 }
 
 rebuildDockerImage() {
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>> Rebuilding the Multi-node Hadoop and Spark Cluster"
-    docker compose down
-    docker rm -f hadoop-master hadoop-slave-1 hadoop-slave-2 spark-master spark-worker-1 spark-worker-2
-    docker image rm -f $DOCKER_IMAGE_NAME
-    docker build -t $DOCKER_IMAGE_NAME .
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>> Rebuilding the Multi-node Hadoop and Spark Cluster"
+  docker compose down
+  docker rm -f hadoop-master hadoop-slave-1 hadoop-slave-2 spark-master spark-worker-1 spark-worker-2
+  docker image rm -f $DOCKER_IMAGE_NAME
+  docker build -t $DOCKER_IMAGE_NAME .
 }
 
-
-if [ $ARG1 == "build" ]
-then
-    buildDockerImage
-elif [ $ARG1 == "deploy" ]
-then
-    deployDockerImage
-elif [ $ARG1 == "publish" ]
-then
-    publishDockerImage
-elif [ $ARG1 ==  "build_and_publish" ]
-then
-    buildDockerImage
-    publishDockerImage
-elif [ $ARG1 ==  "build_and_deploy" ]
-then
-    buildDockerImage
-    deployDockerImage
-elif [ $ARG1 ==  "rebuild_and_deploy" ]
-then
-    rebuildDockerImage
-    deployDockerImage
+if [ "$ARG1" = "build" ]; then
+  buildDockerImage
+elif [ "$ARG1" = "deploy" ]; then
+  deployDockerImage
+elif [ "$ARG1" = "publish" ]; then
+  publishDockerImage
+elif [ "$ARG1" = "build_and_publish" ]; then
+  buildDockerImage
+  publishDockerImage
+elif [ "$ARG1" = "build_and_deploy" ]; then
+  buildDockerImage
+  deployDockerImage
+elif [ "$ARG1" = "rebuild_and_deploy" ]; then
+  rebuildDockerImage
+  deployDockerImage
 else
-   echo "Invalid Args $1"
+  echo "Invalid Args $1"
 fi
-
-
--- multi-node-spark-and-hadoop/spark-hadoop-loader.sh
+++ multi-node-spark-and-hadoop/spark-hadoop-loader.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Settings ENV in .bashrc file
-echo "export JAVA_HOME=/home/bigdata/openlogic-openjdk-8u262-b10-linux-64" >> /root/.bashrc
-echo "export PATH=$PATH:/home/bigdata/openlogic-openjdk-8u262-b10-linux-64/bin" >> /root/.bashrc
+echo "export JAVA_HOME=/home/bigdata/openlogic-openjdk-8u262-b10-linux-64" >>/root/.bashrc
+echo "export PATH=$PATH:/home/bigdata/openlogic-openjdk-8u262-b10-linux-64/bin" >>/root/.bashrc
 source /root/.bashrc
 echo ">>>>>>> Setting ENV variables done..."
 
@@ -11,44 +11,42 @@ echo ">>>>>>> Setting ENV variables done..."
 echo ">>>>>> SSH Server Started Successfully..."
 
 # Start the Master and Worker based on the env params condition
-echo " StrictHostKeyChecking no" >> ~/.ssh/config
+echo " StrictHostKeyChecking no" >>~/.ssh/config
 
 # Delete the configuration tag in xml
 sed -i "/<configuration>/,/<\/configuration>/d " hadoop-2.7.7/etc/hadoop/hdfs-site.xml
 
 MASTER_NODE="master-node"
 
-if [ $NODE_TYPE == "MASTER" ]
-then
-    echo ">>>>>> Starting the Multinode Hadoop Cluster - MASTER NODE..."
-    rm hadoop-2.7.7/etc/hadoop/slaves
-    touch slaves
-    echo "slave-node-1" >> hadoop-2.7.7/etc/hadoop/slaves
-    echo "slave-node-2" >> hadoop-2.7.7/etc/hadoop/slaves
-    cat /home/bigdata/hdfs-site-master.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
-    hadoop-2.7.7/bin/hdfs namenode -format
-    hadoop-2.7.7/sbin/start-all.sh
-    echo ">>>>> Hadoop Master Started Successfully!"
+if [ "$NODE_TYPE" = "MASTER" ]; then
+  echo ">>>>>> Starting the Multinode Hadoop Cluster - MASTER NODE..."
+  rm hadoop-2.7.7/etc/hadoop/slaves
+  touch slaves
+  echo "slave-node-1" >>hadoop-2.7.7/etc/hadoop/slaves
+  echo "slave-node-2" >>hadoop-2.7.7/etc/hadoop/slaves
+  cat /home/bigdata/hdfs-site-master.txt >>hadoop-2.7.7/etc/hadoop/hdfs-site.xml
+  hadoop-2.7.7/bin/hdfs namenode -format
+  hadoop-2.7.7/sbin/start-all.sh
+  echo ">>>>> Hadoop Master Started Successfully!"
 
-    echo ">>>>>> Starting the Multinode Spark Cluster - MASTER NODE..."
-    HOST_MACHINE_IP_ADDRESS=$(hostname -i)
-    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-    ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
-    echo ">>>>> Spark Master Started Successfully!"
+  echo ">>>>>> Starting the Multinode Spark Cluster - MASTER NODE..."
+  HOST_MACHINE_IP_ADDRESS=$(hostname -i)
+  echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >>spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+  ./spark-3.1.2-bin-hadoop2.7/sbin/start-master.sh
+  echo ">>>>> Spark Master Started Successfully!"
 
-elif  [ $NODE_TYPE == "SLAVE" ]
-then
-    echo ">>>>> Starting Hadoop Slave Node - $NODE_NUM..."
-    cat /home/bigdata/hdfs-site-slave.txt >> hadoop-2.7.7/etc/hadoop/hdfs-site.xml
-    echo ">>>>> Hadoop Slave Node - $NODE_NUM Started Successfully"
+elif [ "$NODE_TYPE" = "SLAVE" ]; then
+  echo ">>>>> Starting Hadoop Slave Node - $NODE_NUM..."
+  cat /home/bigdata/hdfs-site-slave.txt >>hadoop-2.7.7/etc/hadoop/hdfs-site.xml
+  echo ">>>>> Hadoop Slave Node - $NODE_NUM Started Successfully"
 
-    echo ">>>>> Starting Spark Slave Node - $NODE_NUM..."
-    HOST_MACHINE_IP_ADDRESS=$(ping $MASTER_NODE -c1 | head -1 | grep -Eo '[0-9.]{4,}')
-    echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >> spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
-    ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
-    echo ">>>>> Spark Slave Node - $NODE_NUM Started Successfully"
+  echo ">>>>> Starting Spark Slave Node - $NODE_NUM..."
+  HOST_MACHINE_IP_ADDRESS=$(ping $MASTER_NODE -c1 | head -1 | grep -Eo '[0-9.]{4,}')
+  echo "SPARK_MASTER_IP=$HOST_MACHINE_IP_ADDRESS" >>spark-3.1.2-bin-hadoop2.7/conf/spark-env.sh
+  ./spark-3.1.2-bin-hadoop2.7/sbin/start-worker.sh $SPARK_MASTER_URL
+  echo ">>>>> Spark Slave Node - $NODE_NUM Started Successfully"
 else
-    echo "Invalid HADOOP NODE_TYPE - $NODE_TYPE"
-fi  
+  echo "Invalid HADOOP NODE_TYPE - $NODE_TYPE"
+fi
 
 tail -f /dev/null
